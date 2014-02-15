b56e1e3ddb31f77cb3ed936b01542906b2ef1d69
Gerrit Grutzeck <gerrit.grutzeck@googlemail.com>
2014-02-15 16:09:11 +0100

    Fixed some errors in the language-files:
    1. Displaying only L instead of Linklist
    2. Contao Translation Tool doesn't like '=>' in language-files
    
    Signed-off-by: Gerrit Grutzeck <g.grutzeck@gfg-development.de>

--- system/modules/linkslist/languages/de/modules.php
+++ system/modules/linkslist/languages/de/modules.php
@@ -11,15 +11,18 @@
 $GLOBALS['TL_LANG']['MOD']['linkslist'][0] = 'Linkliste';
 $GLOBALS['TL_LANG']['MOD']['linkslist'][1] = 'Linkliste verwalten';
 
-$GLOBALS['TL_LANG']['FMD']['linkslist'] = 'Linkslist';
-$GLOBALS['TL_LANG']['FMD']['linkslist_list'] = 'Linkliste';
+$GLOBALS['TL_LANG']['FMD']['linkslist'] = 'Linkliste';
+$GLOBALS['TL_LANG']['FMD']['linkslist_list'] = array('Linkliste', 'Fügt der Seite eine Linklist hinzu');
 
 $GLOBALS['TL_LANG']['tl_module']['sort_legend']     = 'Filter und Sortierung';
 $GLOBALS['TL_LANG']['tl_module']['config_legend']     = 'Moduleinstellungen'; 
 
 $GLOBALS['TL_LANG']['tl_module']['linkslist_sorting'][0] = 'Sortiermodus';
 $GLOBALS['TL_LANG']['tl_module']['linkslist_sorting'][1] = 'Bitte wählen Sie aus, wie die Liste der Links sortiert sein soll';
-$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings'] = array('A' => 'Alphabetisch (Name)', 'B' => 'Alphabetisch (URL)', 'C' => 'Alter aufsteigend', 'D' => 'Alter absteigend');
+$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings']['A'] = "Alphabetisch (Name)";
+$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings']['B'] = "Alphabetisch (URL)";
+$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings']['C'] = "Alter aufsteigend";
+$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings']['D'] = "Alter absteigend";
 
 $GLOBALS['TL_LANG']['tl_module']['linkslist_use_subtitles'][0] = 'Name der Linklisten im Frontend anzeigen';
 $GLOBALS['TL_LANG']['tl_module']['linkslist_use_subtitles'][1] = 'Bitte wählen Sie aus, ob die Namen der Linklisten als Zwischenüberschriften im Frontend angezeigt werden sollen.';
--- system/modules/linkslist/languages/en/modules.php
+++ system/modules/linkslist/languages/en/modules.php
@@ -12,7 +12,7 @@ $GLOBALS['TL_LANG']['MOD']['linkslist'][0] = 'Linklist';
 $GLOBALS['TL_LANG']['MOD']['linkslist'][1] = 'Administrate linklist';
 
 $GLOBALS['TL_LANG']['FMD']['linkslist'] = 'Linklist';
-$GLOBALS['TL_LANG']['FMD']['linkslist_list'] = 'Linklist';
+$GLOBALS['TL_LANG']['FMD']['linkslist_list'] = array('Linklist', 'Adds a Linklist to the page');
 
 $GLOBALS['TL_LANG']['FMD']['linkslist_list'][0] = 'Linklist';
 
@@ -21,7 +21,10 @@ $GLOBALS['TL_LANG']['tl_module']['config_legend']     = 'Modul-Configuration';
 
 $GLOBALS['TL_LANG']['tl_module']['linkslist_sorting'][0] = 'Sorting mode';
 $GLOBALS['TL_LANG']['tl_module']['linkslist_sorting'][1] = 'Select sorting mode';
-$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings'] = array('A' => 'alphabetical (name)', 'B' => 'alphabetical (URL)', 'C' => 'age ascending', 'D' => 'age descending');
+$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings']['A'] = 'alphabetical (name)';
+$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings']['B'] = 'alphabetical (url)';
+$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings']['C'] = 'age ascending';
+$GLOBALS['TL_LANG']['tl_module']['linkslist_sortings']['D'] = 'age descending';
 
 $GLOBALS['TL_LANG']['tl_module']['linkslist_use_subtitles'][0] = "Display linklist's names in the frontend ";
 $GLOBALS['TL_LANG']['tl_module']['linkslist_use_subtitles'][1] = "The linklist's names are displayed as subtitles, if this option is selected.";
